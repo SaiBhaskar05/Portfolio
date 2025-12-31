798394a27c92b71edb7bafee03ed1dc848751fcb
Nanduri Sai Bhaskar <147789704+SaiBhaskar05@users.noreply.github.com>
2025-12-31 08:57:05 +0000

    mail problem fix

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,10 @@ CORS(app)  # allow frontend requests
 # Config
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
